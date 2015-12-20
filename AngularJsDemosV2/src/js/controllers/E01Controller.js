7db7453fd085cd2eff1f74b8abfc25d17449e3c0
mib008 <mib008@126.com>
2015-12-20 21:37:14 +0800

    1220

--- AngularJsDemosV2/src/js/controllers/E01Controller.js
+++ AngularJsDemosV2/src/js/controllers/E01Controller.js
@@ -116,17 +116,17 @@
         
         funcs.es6Functions.push(function () {
             // Not support
-                //function add(...values) {
-                //    let sum = 0;
+                function add(...values) {
+                    let sum = 0;
             
-                //    for (var val of values) {
-                //      sum += val;
-                //   }
+                    for (var val of values) {
+                      sum += val;
+                   }
             
-                //    return sum;
-                //}
+                    return sum;
+                }
             
-                //return add(2, 5, 3); // 10
+                return add(2, 5, 3); // 10
         });
         
         return {
